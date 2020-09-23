13f50d30c4f7465f1ec72adde09482bdff3bc8aa
Shivam Girish Pandey <sgpandey05@gmail.com>
2020-09-23 23:10:43 +0530

    added user profile feed api

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from profiles_api import models
+"""#########################################################"""
 
 class HelloSerializer(serializers.Serializer):
     """serializers a name field for testing our APIview"""
     name = serializers.CharField(max_length=10)
+"""#########################################################"""
 
 class UserProfileSerializer(serializers.ModelSerializer):
     """serializes a user profile obect"""
@@ -35,3 +37,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+"""#########################################################"""
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed items"""
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,11 +6,12 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
+from rest_framework.permissions import IsAuthenticated
 
 from profiles_api import serializers
 from profiles_api import models
 from profiles_api import permissions
-
+"""#########################################################"""
 class HelloApiView(APIView):
 
     #"""test api view"""
@@ -50,6 +51,7 @@ class HelloApiView(APIView):
         """delete object"""
         return Response({'method':'DELETE'})
 
+"""#########################################################"""
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
@@ -98,6 +100,7 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+"""#########################################################"""
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """handle creating and updating profiles"""
@@ -107,7 +110,20 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+"""#########################################################"""
 
 class UserLoginApiView(ObtainAuthToken):
    """Handle creating user authentication tokens"""
    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+"""#########################################################"""
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profile feed items"""
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """Sets the user profile to the logged in user"""
+        serializer.save(user_profile=self.request.user)
