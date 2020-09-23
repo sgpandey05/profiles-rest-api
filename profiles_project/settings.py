425ea445c82ac428218ea1f6645c99a9f4b291e1
Shivam Girish Pandey <sgpandey05@gmail.com>
2020-09-23 23:50:39 +0530

    configured for aws deployment

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7aqg0+h1m(66m&d-5uwhrg^)hw3_!zj51q*pmil=i%hrx24t(d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG',1)))
 
 ALLOWED_HOSTS = []
 
@@ -123,3 +123,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL ='profiles_api.UserProfile'
+
+STATIC_ROOT = 'static/'
